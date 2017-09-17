56ac7c2cb7407bbc9d35c721ff5fc84dff03ae85
nancyzj <ancizj@hotmail.com>
2017-09-17 13:28:12 -0500

    valida que solo sen numeros

--- js/app.js
+++ js/app.js
@@ -1,7 +1,32 @@
 (function(){
+	var primeraUbicacion = $('#primera-ubicacion');
+	var segundaUbicacion = $('#segunda-ubicacion');
+	var numeros = $('.numeros');
+
 	var cargarPagina = function() {
 		$('.modal').modal();
+		primeraUbicacion.keydown(validarNumeros);
+		segundaUbicacion.keydown(validarNumeros)
 	}
+
+	/*var validarDatos = function(event) {
+		var expresionRegular = /^[0-9]$/;
+
+		numeros.each(function( indice, elemento) {
+			// console.log(indice , elemento);
+			var input = $(elemento);
+			if(expresionRegular.test(input.val())){
+				console.log('no es numero');
+			}else{
+				console.log('es numero');
+			}
+		});
+	}*/
+	var validarNumeros = function(e){
+		if (e.keyCode !== 8 && (e.keyCode < 48 || e.keyCode > 57)){
+			e.preventDefault();
+		}	
+	};
 	
 	$(document).ready(cargarPagina);
 })();
