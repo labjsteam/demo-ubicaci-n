d224824946ce0bb0c0430606d03ff6c26ef5c6d7
nancyzj <ancizj@hotmail.com>
2017-09-18 00:27:28 -0500

    implementa el mapa

--- js/app.js
+++ js/app.js
@@ -1,32 +1,51 @@
 (function(){
-	var primeraUbicacion = $('#primera-ubicacion');
-	var segundaUbicacion = $('#segunda-ubicacion');
-	var numeros = $('.numeros');
+	var latitud = $('#latitud');
+	var longitud = $('#longitud');
+	var btnMostrarLugarFavorito = $('#mostrarLugarFavorito');
 
 	var cargarPagina = function() {
 		$('.modal').modal();
-		primeraUbicacion.keydown(validarNumeros);
-		segundaUbicacion.keydown(validarNumeros)
+		// latitud.keydown(validarNumeros);
+		// longitud.keydown(validarNumeros);
+		// comprobarNavegador();
+		btnMostrarLugarFavorito.click(initMap);
+	}
+	// var validarNumeros = function(e){
+	// 	if (e.keyCode !== 8 && (e.keyCode < 48 || e.keyCode > 57)){
+	// 		e.preventDefault();
+	// 	}	
+	// };
+	// var comprobarNavegador = function() {
+	// 	if ("geolocation" in navigator) {
+	// 		// obtenerUbicacion es nuestra primera funcion como parametro
+	// 		navigator.geolocation.getCurrentPosition(initMap, function(error) {
+	// 			console.log(error);
+	// 		});
+	// 	} else {
+	// 		alert('Tu navegador no soporta el GPS');
+	// 	}
+	// }
+	var initMap = function() {
+		var valorLatitud = parseInt(latitud.val());
+		var valorLongitud = parseInt(longitud.val());
+		console.log(typeof valorLatitud);
+		console.log( typeof valorLongitud);
+
+        var misCoordenadas = {
+	        	// lat: -25.363,
+	        	// lng: 131.044
+	        	lat: valorLatitud,
+	        	lng: valorLongitud
+        	};
+        var map = new google.maps.Map($('#mapa')[0], {
+          zoom: 4,
+          center: misCoordenadas
+        });
+        var marker = new google.maps.Marker({
+          position: misCoordenadas,
+          map: map
+        });
 	}
 
-	/*var validarDatos = function(event) {
-		var expresionRegular = /^[0-9]$/;
-
-		numeros.each(function( indice, elemento) {
-			// console.log(indice , elemento);
-			var input = $(elemento);
-			if(expresionRegular.test(input.val())){
-				console.log('no es numero');
-			}else{
-				console.log('es numero');
-			}
-		});
-	}*/
-	var validarNumeros = function(e){
-		if (e.keyCode !== 8 && (e.keyCode < 48 || e.keyCode > 57)){
-			e.preventDefault();
-		}	
-	};
-	
 	$(document).ready(cargarPagina);
 })();
