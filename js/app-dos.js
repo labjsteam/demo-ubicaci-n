81c00d61fe187efb38656637dc444be4cd869021
Jesus Rosas <infinitdeveloperofficial@gmail.com>
2017-09-22 01:21:47 -0500

    Fix: Coords Personal and Rename functions

--- js/app-dos.js
+++ js/app-dos.js
@@ -1,10 +1,20 @@
 (function(){
+	
+	// Defines variables globales
 	var latitud = $('#latitud');
 	var longitud = $('#longitud');
 	var btnMostrarLugarFavorito = $('#mostrarLugarFavorito');
 	var btnUbicacionActual = $('#btn-ubicacion-actual');
 	var coordenadasActuales;
-	var cargarPagina = function() {
+	var misCoordenadas;
+
+
+
+	/**
+	 * Como referencia las funciones nombralas de la forma function name(){} para evitar confusión
+	 */	
+
+	function cargarPagina() {
 		$('.modal').modal();
 		latitud.keydown(validarNumeros);
 		longitud.keydown(validarNumeros);
@@ -13,12 +23,14 @@
 		btnMostrarLugarFavorito.click(initMap);
 		btnUbicacionActual.click(comprobarNavegador);
 	}
-	var validarNumeros = function(e){
+
+
+	function validarNumeros(e){
 		if (e.keyCode !== 8 && (e.keyCode < 48 || e.keyCode > 57) && (e.keyCode < 189 || e.keyCode > 190)){
 			e.preventDefault();
 		}	
 	};
-	var comprobarNavegador = function() {
+	function comprobarNavegador() {
 		if ("geolocation" in navigator) {
 			// obtenerUbicacion es nuestra primera funcion como parametro
 			console.log('si acepta tu navegador el GPS');
@@ -30,30 +42,26 @@
 		}
 	}
 
-	var initMap = function() {
+	function initMap() {
 		// lat y lng solo recibe numeros, por eso la conversion
 		var valorLatitud = Number(latitud.val());
 		var valorLongitud = Number(longitud.val());
         misCoordenadas = {
-	        	// lat: -25.363,
-	        	// lng: 131.044
-	        	lat: valorLatitud,
-	        	lng: valorLongitud
-        	};   
+			lat: valorLatitud,
+			lng: valorLongitud
+        };
         var map = new google.maps.Map($('#mapa')[0], {
-          zoom: 4,
-          center: misCoordenadas
+        	zoom: 4,
+        	center: misCoordenadas
         });
         var marker = new google.maps.Marker({
-          position: misCoordenadas,
-          map: map
+        	position: misCoordenadas,
+        	map: map
         });
-        return misCoordenadas;
-     	// console.log(misCoordenadas);
+
 	};
 
-	var ubicacionActual = function(posicion){
-		// console.log(posicion);
+	function ubicacionActual(posicion){
 		coordenadasActuales = {
 			lat: posicion.coords.latitude, 
 			lng: posicion.coords.longitude
@@ -61,7 +69,7 @@
 		mostrarMapaActual(coordenadasActuales);
 	}
 
-	var mostrarMapaActual = function( coordenadasActuales) {
+	function mostrarMapaActual(coordenadasActuales) {
 		var btnRuta = $('#btnRuta');
 		var mapa = new google.maps.Map($('#mapa-ubicacion-actual')[0], {
 			zoom: 18,
@@ -77,54 +85,41 @@
 		botonRuta.text("mostrar ruta");
 		// botonRuta.click(trazarRuta);
 		botonRuta.on('click', function() {
-            trazarRuta(coordenadasActuales);
+
+			// Anteriormente solo tenías un parametro, recuerda que recibe dos.
+            trazarRuta(misCoordenadas,coordenadasActuales);
         })
 		btnRuta.append(botonRuta);
-		return coordenadasActuales;
 	}
 
-	var trazarRuta = function(misCoordenadas, coordenadasActuales) {
-		var puntoUno = initMap(misCoordenadas);	
+	function trazarRuta(misCoordenadas, coordenadasActuales) {
+		
+		// Almacenas en variables independientes lo que recibes por parámetros
+		// Como parámetro recibes dos objetos, anteriormente recibias uno
+		var puntoUno = misCoordenadas;	
 		var puntoDos = coordenadasActuales;
-		console.log(puntoUno, puntoDos);
-		// console.log(misCoordenadas, coordenadasActuales);
-		// console.log(puntoUno);
 
 
-		// var coordenadasPuntoUno = {
-		// 	lat: puntoUno.lat,
-		// 	lng: puntoUno.lng
-		// };		
-		// console.log('las rutas' + coordenadasPuntoUno.lat + coordenadasPuntoUno.lng);
-		// mapa.drawRoute({
-		// 	origin: [coordenadasRuta.lat, coordenadasRuta.lng],
-		// 	destination: [valorLatitud, valorLongitud],
-		// 	travelMode: 'driving',
-		// 	strokeColor: '#131540',
-		// 	strokeOpacity: 0.6,
-		// 	strokeWeight: 6
-		// });
-		// var misCoordenadas = {
-	 //        	lat: -25.363,
-	 //        	lng: 131.044
-  //       	};
-		// var mapita = new google.maps.Map($('#mapa-ubicacion-actual')[0], {
-		// 	zoom: 8,
-		// 	center: misCoordenadas
-		// });
-	      // var mapita = new GMaps({
-	      //   el: '#mapa-ubicacion-actual',
-	      //   lat: -12.044012922866312,
-	      //   lng: -77.02470665341184
-	      // });
-	      // mapita.drawRoute({
-	      //   origin: [-12.044012922866312, -77.02470665341184],
-	      //   destination: [-12.090814532191756, -77.02271108990476],
-	      //   travelMode: 'driving',
-	      //   strokeColor: '#131540',
-	      //   strokeOpacity: 0.6,
-	      //   strokeWeight: 6
-	      // });
+		// Para pintar la ruta solo necesitarias pasar el origen y el destino (la variable puntoUno y Dos)
+
+
+		/*  mapa.drawRoute({
+		 	origin: [coordenadasRuta.lat, coordenadasRuta.lng],
+		 	destination: [valorLatitud, valorLongitud],
+		 	travelMode: 'driving',
+		 	strokeColor: '#131540',
+		 	strokeOpacity: 0.6,
+		 	strokeWeight: 6
+		 });
+
+	       mapita.drawRoute({
+	         origin: [-12.044012922866312, -77.02470665341184],
+	         destination: [-12.090814532191756, -77.02271108990476],
+	         travelMode: 'driving',
+	         strokeColor: '#131540',
+	         strokeOpacity: 0.6,
+	         strokeWeight: 6
+	       }); */
 		}
 
 	$(document).ready(cargarPagina);
